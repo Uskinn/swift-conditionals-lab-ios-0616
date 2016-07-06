5f99b35120a0c823e9a293842e10207b7302f85a
Uskinn <s.nevzor@yahoo.com>
2016-07-06 15:35:22 -0400

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -26,45 +26,56 @@ let b = 32
 */
 // write your code here
 
+print(a >= b)
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
+print(a % b == 0)
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
 
+    print(y * b <= a)
+
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
-
+print (a <= b)
 
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
+let isTrue = true
+let isFalse = false
 
-
-/*: Question 6
-### 6. Print "true" if a divided by b is greater than x
-*/
+if (a % b == 0) {
+    print(isTrue)
+}
 // write your code here
 
 
+if (Double(a / b) > x) {
+    print(isTrue)
+}
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
 
-
+if (y / Int(x)) > 3 {
+    print(isTrue)
+} else {
+    print(isFalse)
+}
 
 
 /*: Question 8
@@ -72,14 +83,24 @@ let b = 32
 */
 // write your code here
 
-
+if (y > Int(x)) && (a / b) > 9 {
+    print(isTrue)
+}
 
 
 /*: Question 9
 ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
 */
 // write your code here
-
+func isGreater(a: Int, b: Int) -> Bool {
+    if (a > b) {
+        return isTrue
+    } else {
+        
+        return isFalse
+    }
+    
+}
 
 
 
@@ -89,7 +110,16 @@ let b = 32
 ### 10. write a function "isForceWith" that takes a String argument and returns true if the argument is the name of some whom the force is strong with, otherwise it returns false.  People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
 */
 // write your code here
-
+func isForceWith(name: String) -> Bool {
+    
+    switch name {
+    case "Luke", "Leia", "Anakin", "Anakin", "Yoda", "Vader":
+        return true
+    default:
+        return false
+    }
+    
+}
 
 
 /*: Question 11
@@ -97,6 +127,14 @@ let b = 32
 */
 // write your code here
 
+func bankAccount(myaccount: Int, someAccount: Int) {
+    
+    if someAccount >= 10 {
+        
+        someAccount - 10
+        myaccount + 10
+    }
+}
 
 
 
